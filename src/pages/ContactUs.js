e387b7de6a037236d8cd89b94970deba9b56d626
unknown <rishabdutt4@gmail.com>
2026-02-14 11:36:53 +0530

    14/feb/2026

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -66,16 +66,17 @@ title="Contact US"
 
         {/* GOOGLE MAP */}
         <div className="contact-us-map">
-          <iframe
-            title="Google Map"
-            src="https://www.google.com/maps?q=Bangalore%20Karnataka%20India&output=embed"
-            width="100%"
-            height="400"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+         <iframe
+  title="Google Map"
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3516.9468582304517!2d79.55748887531833!3d28.178531575915635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399ffb42d55f941d%3A0x6d9c386e52531212!2sSantosh%20Polyfab%20Private%20Limited!5e0!3m2!1sen!2sin!4v1771049070375!5m2!1sen!2sin"
+  width="100%"
+  height="400"
+  style={{ border: 0 }}
+  allowFullScreen
+  loading="lazy"
+  referrerPolicy="no-referrer-when-downgrade"
+/>
+
         </div>
       </Container>
     </section>
