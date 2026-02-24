03262272e6fb0bd4ceaa209f54b1692d94dfaf70
unknown <rishabdutt4@gmail.com>
2026-02-24 18:32:29 +0530

    24/feb/2026

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -5,26 +5,26 @@ import { motion, AnimatePresence } from "framer-motion";
 import "./HomeBanner.css";
 
 const slides = [
-  {
-    image: require("../assets/Banner/factory1.png"),
-    title: "Reliable Packaging Materials, Built to Perform",
-    desc: "At Santosh Polyfab, we manufacture PP fabric, PP bags, monolayer films, multifilament yarn, and reprocess granules with uncompromising quality.",
-  },
-  {
-    image: require("../assets/Banner/factory2-blue.png"),
-    title: "High Quality PP Bags for Every Industry",
-    desc: "Our PP bags are designed for strength, durability and long-lasting performance across multiple industries.",
-  },
-  {
-    image: require("../assets/Banner/factory3-blue.png"),
-    title: "Sustainable & Strong Manufacturing",
-    desc: "We focus on eco-friendly processes while maintaining premium quality and consistency in every product.",
-  },
-  {
-    image: require("../assets/Banner/factory-solor.png"),
-    title: "Sustainable & Strong Manufacturing",
-    desc: "We focus on eco-friendly processes while maintaining premium quality and consistency in every product.",
-  },
+{
+  image: require("../assets/Banner/factory1.png"),
+  title: "Reliable Packaging Materials, Built to Perform",
+  desc: "At Santosh Polyfab, we manufacture PP fabric, PP bags, monolayer films, multifilament yarn, and reprocess granules with uncompromising quality.",
+},
+{
+  image: require("../assets/Banner/factory2-blue.png"),
+  title: "High Quality PP Bags for Every Industry",
+  desc: "Our PP bags are designed for strength, durability and long-lasting performance across multiple industries.",
+},
+{
+  image: require("../assets/Banner/factory3-blue.png"),
+  title: "Sustainable & Strong Manufacturing",
+  desc: "We focus on eco-friendly processes while maintaining premium quality and consistency in every product.",
+},
+{
+  image: require("../assets/Banner/factory-solor.png"),
+  title: "Powered by Clean & Efficient Energy",
+  desc: "Our solar-powered infrastructure supports sustainable manufacturing while reducing environmental impact and ensuring efficient production.",
+},
 ];
 
 const HomeBanner = () => {
--- src/pages/Industry.js
+++ src/pages/Industry.js
@@ -67,7 +67,7 @@ title="Industries"
     <Container fluid className="industry-section">
       <Container className="industry-heading-para-div">
 
-  <h1 className="page-heading">Our Esteemed Customers</h1>
+  <h1 className="page-heading">What We Offer..?</h1>
   <p className="page-description">
     Over the years, Santosh Polyfab has earned the trust of leading organizations
     by delivering high-quality packaging materials and dependable service. We
